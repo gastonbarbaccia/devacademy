e078a9418ac66ed66e94c274d365214db6948a11
Gaston <gastonbarbaccia@hotmail.com>
2020-03-24 23:06:53 -0300

    Se agregan las pagina de los cursos

--- enviar.php
+++ enviar.php
@@ -1,18 +1,19 @@
 <?php
 
-
 $to = "gastonbarbaccia@hotmail.com";
 $from = $_POST['nombre'];
 $email = $_POST['email'];
 $subject = $_POST['asunto'];
+$message = $_POST['mensaje'];
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/plain;charset=UTF-8" . "\r\n";
+$headers .= "From: DevAcademy < contacto@devacademy.com >" . "\r\n";
+
  
-$message = $_POST['mensaje'];
- 
-mail($to,$email, $subject, $message, $headers);
+mail($to,$subject, $message, $headers);
 
 echo(1); 
+
 
 
 ?>
